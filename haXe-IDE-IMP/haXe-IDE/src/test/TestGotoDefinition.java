b2e034179e2ef5c8c31b1b5615b653bd700058db
victor.polozov <victor.polozov@02e23126-1357-11df-83e8-2f775389245c>
2010-11-20 21:16:50 +0000

    rename test file

---
+++ haXe-IDE-IMP/haXe-IDE/src/test/TestGotoDefinition.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Anatoly Kondratyev (anatoly.kondratyev@googlemail.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU General Public License, version 2
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Contributors:
+ *    Anatoly Kondratyev (anatoly.kondratyev@googlemail.com)
+ *******************************************************************************/
+package test;
+
+import static junit.framework.Assert.assertEquals;
+import static test.TestHelper.parseHaxeFile;
+import haxe.imp.parser.antlr.tree.ExtendedCommonTree;
+import haxe.imp.parser.antlr.tree.specific.ClassNode;
+import haxe.imp.parser.antlr.tree.specific.FunctionNode;
+import haxe.imp.parser.antlr.tree.specific.VarDeclaration;
+
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+
+public class TestGotoDefinition {
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetDeclarationNode() throws RecognitionException {
+		ExtendedCommonTree tree = parseHaxeFile("testGOTODef01.hx");
+		ExtendedCommonTree iDecl = tree.getNodeByPosition(12, 23)
+				.getDeclarationNode(tree.getNodeByPosition(12, 23));
+		assertEquals(11, iDecl.getLine());
+		assertEquals(2, iDecl.getCharPositionInLine());
+		assertEquals("i", iDecl.getChild(0).getText());
+		assertEquals(VarDeclaration.class, iDecl.getClass());
+
+		iDecl = tree.getNodeByPosition(11, 14).getDeclarationNode(
+				tree.getNodeByPosition(11, 14));
+		assertEquals(9, iDecl.getLine());
+		assertEquals(29, iDecl.getCharPositionInLine());
+		assertEquals("tmp", iDecl.getChild(0).getText());
+		assertEquals(VarDeclaration.class, iDecl.getClass());
+
+		iDecl = tree.getNodeByPosition(13, 2).getDeclarationNode(
+				tree.getNodeByPosition(13, 2));
+		assertEquals(7, iDecl.getLine());
+		assertEquals(1, iDecl.getCharPositionInLine());
+		assertEquals("foo", iDecl.getChild(0).getText());
+		assertEquals(VarDeclaration.class, iDecl.getClass());
+
+		iDecl = tree.getNodeByPosition(14, 2).getDeclarationNode(
+				tree.getNodeByPosition(14, 2));
+		assertEquals(17, iDecl.getLine());
+		assertEquals(1, iDecl.getCharPositionInLine());
+		assertEquals("bar", iDecl.getChild(0).getText());
+		assertEquals(VarDeclaration.class, iDecl.getClass());
+	}
+
+	/**
+	 * Test get function declaration node.
+	 * @throws RecognitionException 
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetFunctionDeclarationNode() throws RecognitionException {
+		ExtendedCommonTree tree = parseHaxeFile("testGOTODef02.hx");
+		ExtendedCommonTree iDecl = tree.getNodeByPosition(9, 2)
+				.getDeclarationNode(tree.getNodeByPosition(9, 2));
+		assertEquals(12, iDecl.getLine());
+		assertEquals(1, iDecl.getCharPositionInLine());
+		assertEquals("tmpFunct", iDecl.getChild(0).getText());
+		assertEquals(FunctionNode.class, iDecl.getClass());
+
+		iDecl = tree.getNodeByPosition(14, 2).getDeclarationNode(
+				tree.getNodeByPosition(14, 2));
+		System.out.println(iDecl);
+		assertEquals(12, iDecl.getLine());
+		assertEquals(1, iDecl.getCharPositionInLine());
+		assertEquals("tmpFunct", iDecl.getChild(0).getText());
+		assertEquals(FunctionNode.class, iDecl.getClass());
+	}
+
+	/**
+	 * Test get class declaration node.
+	 * @throws RecognitionException 
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetClassDeclarationNode() throws RecognitionException {
+		ExtendedCommonTree tree = parseHaxeFile("testGOTODef03.hx");
+
+		ExtendedCommonTree iDecl = tree.getNodeByPosition(9, 2)
+				.getDeclarationNode(tree.getNodeByPosition(9, 2));
+		System.out.println(iDecl);
+		assertEquals(3, iDecl.getLine());
+		assertEquals(0, iDecl.getCharPositionInLine());
+		assertEquals("Main", iDecl.getChild(0).getText());
+		assertEquals(ClassNode.class, iDecl.getClass());
+	}
+}

a190a55983d58a688096b1c4c8d3f9e3efdbf41b
piranuy <piranuy@02e23126-1357-11df-83e8-2f775389245c>
2011-03-21 17:00:38 +0000

    Good version of simple game - still have some things to do, but already playable.

--- haXe simple game/Server.hx
+++ haXe simple game/Server.hx
@@ -8,7 +8,8 @@ class ClientData implements ServerApi {
 	var api : ClientApiImpl;
 	var name : String;	
 	var myMark: Bool;
-	var ifMyTurn : Bool;
+	var ifMyTurn : Bool;	
+	var gameField :Array<Int>; 
 
 	public function new( scnx : haxe.remoting.SocketConnection ) {
 		api = new ClientApiImpl(scnx.client);
@@ -19,25 +20,35 @@ class ClientData implements ServerApi {
 		if( this.name != null )
 			throw "You are already identified";
 		this.name = name;
-		Server.clients.add(this);
+		gameField = new Array<Int>();	
+		
+		for (i in 0...9)
+			gameField[i] = 2;
 		
 		//true Mark = X, false Mark = O, others just watching
-		if (Server.clients.length > 2){
-			ifMyTurn = false;
+		if (Server.players.length == 0){
+			Server.players.add(this);
 			myMark = true;
+			ifMyTurn = true;
+			this.api.userAddMouseEvents();
 		}
-			else if (Server.clients.length == 2){
-				myMark = false;
-				ifMyTurn = false;
-				this.api.userAddMouseEvents();
-			}
-				else {
-					this.api.userAddMouseEvents();
-					ifMyTurn = true;
-					myMark = true;
-				}
+		else if (Server.players.length == 1) {
+			Server.players.add(this);
+			myMark = false;
+			ifMyTurn = false;
+			this.api.userAddMouseEvents();
+		}
+		else {
+			Server.clients.add(this);
 			
-		for( c in Server.clients ) {
+		}
+		
+		joinUsers(Server.players);
+		joinUsers(Server.clients);
+	}
+	
+	public function joinUsers(clientList:List<ClientData>) {
+		for( c in clientList ) {
 			if( c != this ){
 				c.api.userJoin(name);	
 				c.api.userWriteStatistics(makeList());
@@ -51,20 +62,13 @@ class ClientData implements ServerApi {
 		var text:String = "";
 		var i:Int = 1;
 		
-		if (Server.clients.length == 1)
-			text = name;
-		else 
-			for ( c in Server.clients ) {
-				if (i == 3)
-					text += "<br>Watching:<br>";
-				if( c != this ){
-					text = text + c.name + "<br>";	
-					i++;
-				} else {
-				text = text + name + "<br>";
-				i++;
-				}
-			}	
+		for ( c in Server.players)
+			text += c.name + " ("+((c.getMark())?"X":"O")+")" + "<br>";
+		if ( Server.clients.length > 0) {
+			text += "<br>Watching:<br>";
+			for ( c in Server.clients )
+				text += c.name + "<br>";
+		}
 			
 		return text;
 	}
@@ -77,26 +81,52 @@ class ClientData implements ServerApi {
 		ifMyTurn = !ifMyTurn;
 	}
 	
-	public function makeMove(x:Float, y:Float) {
-		if (ifMyTurn) {			
-			for( c in Server.clients ){
-				c.api.userMakeMove(myMark,x, y);
-				c.switchTurn();
-				say("<b>"+c.name+"</b>" + " made turn");
-			}
+	public function makeMove(x:Float, y:Float,coordX:Int, coordY:Int) {
+		if (Server.players.length > 1){
+			if (ifMyTurn) {
+				if (gameField[coordX*3+coordY] == 2) {					
+					changeTable(coordX,coordY);
+					for( c in Server.clients )
+						c.api.userMakeMove(myMark,x, y);
+					for (c in Server.players) {
+						c.api.userMakeMove(myMark,x, y);
+						c.switchTurn();
+					}
+					say("Made his move.");
+				}
+			} else
+			api.userSay(name, "It's not you turn now.");
 		}
-		say("<b>"+name+"</b>" + " - it's not you turn");
+	}
+	
+	public function changeTable(x:Int, y:Int) {
+		for( c in Server.players )		
+			c.gameField[x*3+y] = (myMark)?1:0;
+		for( c in Server.clients )
+			c.gameField[x*3+y] = (myMark)?1:0;
 	}
 
 	public function say( text : String ) {
+		for( c in Server.players )
+			c.api.userSay(name, text);
 		for( c in Server.clients )
-			c.api.userSay(name,text);
+			c.api.userSay(name, text);
 	}
 
 	public function leave() {
-		if( Server.clients.remove(this) )
+		var success:Bool = Server.clients.remove(this) || Server.players.remove(this);
+		if( success ){
+			if (Server.players.length < 2 )
+				if (Server.clients.length > 1){
+				Server.players.add(Server.clients.pop());
+				Server.players.last().ifMyTurn = !Server.players.first().ifMyTurn;
+				Server.players.last().myMark = !Server.players.first().getMark();
+			}
 			for( c in Server.clients )
 				c.api.userLeave(name);
+			for( c in Server.players )
+				c.api.userLeave(name);
+		}
 	}
 
 	public static function ofConnection( scnx : haxe.remoting.SocketConnection ) : ClientData {
@@ -110,7 +140,7 @@ class ClientData implements ServerApi {
 class Server {
 
 	public static var clients = new List<ClientData>();
-	public static var gameField = new Array<Array<Int>>();
+	public static var players = new List<ClientData>();
 
 	static function initClientApi( scnx : haxe.remoting.SocketConnection, context : haxe.remoting.Context ) {
 		trace("Client connected");
@@ -129,14 +159,6 @@ class Server {
 		var s = new neko.net.ThreadRemotingServer(domains);
 		s.initClientApi = initClientApi;
 		s.clientDisconnected = onClientDisconnected;
-		
-		for (i in 1...3) {
-				var xxx = new Array<Int>();
-				xxx.push(2);
-				xxx.push(2);
-				xxx.push(2);
-				gameField.push(xxx);
-			}
 		
 		trace("Starting server...");
 		s.run(host,1024);

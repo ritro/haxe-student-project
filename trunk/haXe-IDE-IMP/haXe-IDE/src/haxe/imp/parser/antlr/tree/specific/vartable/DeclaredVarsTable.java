71d757fae28298b7530da47266c55ba5477bd46b
piranuy@gmail.com <piranuy@gmail.com@02e23126-1357-11df-83e8-2f775389245c>
2011-10-16 20:27:30 +0000

    Several comments inserted and readability of code improved. Now using jdk 1.7

--- trunk/haXe-IDE-IMP/haXe-IDE/src/haxe/imp/parser/antlr/tree/specific/vartable/DeclaredVarsTable.java
+++ trunk/haXe-IDE-IMP/haXe-IDE/src/haxe/imp/parser/antlr/tree/specific/vartable/DeclaredVarsTable.java
@@ -1,6 +1,7 @@
 package haxe.imp.parser.antlr.tree.specific.vartable;
 
 import haxe.imp.parser.antlr.tree.HaxeTree;
+import haxe.imp.parser.antlr.tree.exceptions.VarAlreadyDeclaredException;
 import haxe.imp.parser.antlr.tree.specific.BlockScopeNode;
 import haxe.imp.parser.antlr.tree.specific.ReturnNode;
 import haxe.imp.parser.antlr.tree.specific.vartable.VarDeclaration.VarType;
@@ -10,149 +11,175 @@ import java.util.ArrayList;
 
 import org.antlr.runtime.CommonToken;
 
+/**
+ * Table that contains different declarations (e.g class, function, enums)
+ * and usages (e.g. variables) useful in type interference.
+ * @author Savenko Maria
+ *
+ */
 public class DeclaredVarsTable {
 
     private ArrayList<VarDeclaration> declaredVars = new ArrayList<VarDeclaration>();
 
-    public ArrayList<VarDeclaration> getDeclaredVars() {
+    public ArrayList<VarDeclaration> getDeclaredVars()
+    {
         return declaredVars;
     }
 
-    public boolean ifVarExists(VarDeclaration var) {
-        for (VarDeclaration tree : declaredVars) {
+    /**
+     * Checks if declaration for var, class or function
+     * already exists in the table.
+     * @param var
+     * @return true if table contains variable
+     */
+    public boolean contains(VarDeclaration var)
+    {
+        for (VarDeclaration tree : declaredVars)
+        {
+            //we look only for declarations
+            if (tree instanceof VarUse)
+            {
+                continue;
+            }
             if (tree.getToken().equals(var.getToken()))
-                break; // looking for the previous vars with the same text
-            if (tree instanceof FunctionDeclaration) {
-                if (var.getText().equals(((FunctionDeclaration) tree).getText()) &&
-                        // FIXME && parametres equals... ??
-                    !var.getToken().equals(((FunctionDeclaration) tree).getToken()))
+                break; // looking for the previous vars with the same text FIXME????
+            if (tree instanceof FunctionDeclaration 
+                    && var.getText().equals(((FunctionDeclaration) tree).getText()) 
+                    // FIXME && parameters equals... ??
+                    && !var.getToken().equals(((FunctionDeclaration) tree).getToken()))
+            {
                     return true; // another var with the same name
-            } else if (!(tree instanceof VarUse)) {
-                if (var.getText().equals(tree.getText()) && 
-                    var.getDeclType().equals(tree.getDeclType()) && // now just for param-decl override 
-                    //&& parametres equals... && blockscope mark equals
-                    !var.getToken().equals(tree.getToken()))
-                    return true; // another var with the same name
+            }
+            if (!(tree instanceof FunctionDeclaration)
+                    && var.getText().equals(tree.getText())
+                    //&& parameters equals... && blockscope mark equals
+                    && !var.getToken().equals(tree.getToken()))
+            {
+                return true; // another var with the same name
             }
         }
         return false;
     }
     
-    public void addWithIncrease(VarDeclaration element){
-        if ((element.getDeclType() == VarType.FunctionParam 
-                    || element.getDeclType() == VarType.FunctionVarDecl) 
-                    && this.findVarNumber(element.getText()) != -1) 
-            element.setVarNumber(this.findVarNumber(element.getText()) + element.getVarNumber() + 1);
-        
-        this.addToDeclaredVars(element);
-    }
-    
-    public void addWithIncrease(VarUse element){
-        if (this.findVarNumber(element.getText()) != -1)
-            element.setVarNumber(this.findVarNumber(element.getText()));
-        
-        this.addToDeclaredVars(element);
-    }
-    
-    public void addWithIncrease(DeclaredVarsTable table){
-        ArrayList<String> increased = new ArrayList<String>();
-        
-        for (VarDeclaration x: table.getDeclaredVars()){
-            if ((x.getDeclType() == VarType.FunctionParam 
-                    || x.getDeclType() == VarType.FunctionVarDecl) 
-                    && this.findVarNumber(x.getText()) != -1
-                    && !increased.contains(x.getText())) 
-                table.increaseVarNumber(this.findVarNumber(x.getText()) + 1, 
-                        x.getVarNumber(), x.getText());
-                increased.add(x.getText());
-        }
-        
-        this.addAll(table);
-    }
-    
-    private void increaseVarNumber(int onHow, int fromNumber, String varName){
-        for (VarDeclaration x : getDeclaredVars()){
-            if ( x.getText().equals(varName)
-                    && x.getVarNumber() >= fromNumber){
-                x.setVarNumber(x.getVarNumber() + onHow);
-            }
-            if (x instanceof VarUse 
-                    && ((VarUse)x).getAssignExpr() != null
-                    && ((VarUse)x).getAssignExpr().getText().equals(varName)
-                    && ((VarUse)x).getAssignExpr().getVarNumber() >= fromNumber){
-                ((VarUse)x).getAssignExpr().setVarNumber(x.getVarNumber() + onHow);
-            } 
-        }
-    }
-    
-    private int findVarNumber(String name){
-        int maxNum = -1;
-        for (VarDeclaration x: getDeclaredVars())
-            if (x.getText().equals(name)
-                    && maxNum < x.getVarNumber())
-                maxNum = x.getVarNumber();
-        
-        return maxNum;
-    }
-
-    public VarDeclaration findDeclaredVar(CommonToken declToken) {
-        for (VarDeclaration x : getDeclaredVars())
-            if (x.getToken().equals(declToken))
-                return x;
-        for (VarDeclaration x : getDeclaredVars())
-            if (x instanceof ClassDeclaration)
-                return x.getDeclaredVars().findDeclaredVar(declToken);
-        return null;
-    }
-    
-    public VarDeclaration findDeclaredVar(String name) {
-        for (VarDeclaration x : getDeclaredVars())
-            if (x.getText().equals(name)
-                    && x.ifVarDeclaration())
-                return x;
-        for (VarDeclaration x : getDeclaredVars())
-            if (x instanceof ClassDeclaration)
-                return x.getDeclaredVars().findDeclaredVar(name);
-        return null;
-    }
-
-    /*
-     * public VarDeclNode findDeclaredVar(String name, CommonToken
-     * blockScopeToken){ for (VarDeclNode x: getDeclaredVars()) if
-     * (x.getScopeToken().equals(blockScopeToken)) return x; for (VarDeclNode x:
-     * getDeclaredVars()) if (x instanceof ClassDeclNode) return
-     * x.getDeclaredVars().findDeclaredVar(token); return null; }
+    /**
+     * Experimental function with immediate error effect.
+     * If overlay is allowed it will search for previous 
+     * declaration and adds this one with new number = old + 1.
+     * Else it will throw exception if such variable already was
+     * declared.
      */
-    
-    public void setDeclaredVarType(String name,int Num, HaxeType type) {
-        for (VarDeclaration x : getDeclaredVars()) {
-            if (Num == x.getVarNumber()
-                    && x.getText().equals(name)
-                    && !x.getDeclType().equals(VarType.ClassVarDecl))
-                x.setHaxeType(type);
+    public void tryAdd(VarDeclaration element) 
+            throws VarAlreadyDeclaredException
+    {
+        int foundVarNumber = findVarNumber(element.getText());
+        if (foundVarNumber == -1)
+        {
+            add(element);
+            return;
         }
+        if (element.getDeclType() == VarType.FunctionParam
+                || element.getDeclType() == VarType.FunctionVarDecl)
+        {
+            int newVarNumber = foundVarNumber + 1;
+            element.setVarNumber(newVarNumber);
+            add(element);
+            return;
+        }
+        if (contains(element))
+        {
+            throw new VarAlreadyDeclaredException();
+        }
+        add(element);
+    }
+    
+    /**
+     * If declaration is inside the function or it is a function
+     * parameter then variable number will be increased to
+     * determine the variable overlay. For over types of declaration
+     * var number will not be changed.
+     * @param Declaration to add to the table.
+     */
+    public void addWithIncrease(VarDeclaration element)
+    {
+        if ((element.getDeclType() == VarType.FunctionParam
+                || element.getDeclType() == VarType.FunctionVarDecl)
+                && findVarNumber(element.getText()) != -1)
+        {
+            int newVarNumber = findVarNumber(element.getText())
+                    + element.getVarNumber() + 1;
+            element.setVarNumber(newVarNumber);
+        }
+
+        this.add(element);
+    }
+    
+    /**
+     * Finds the last declaration for that var and adds var to
+     * table with that declaration var number. If no declaration
+     * was found the element will be added untouched.
+     * @param Variable usage to add to the table.
+     */
+    public void addWithIncrease(VarUse element)
+    {
+        int foundVarNumber = findVarNumber(element.getText());
+        if (foundVarNumber != -1)
+        {
+            element.setVarNumber(foundVarNumber);
+        }
+
+        this.add(element);
+    }
+    
+    public void addWithIncrease(DeclaredVarsTable table)
+    {
+        ArrayList<String> checked = new ArrayList<String>();
+
+        for (VarDeclaration x : table.getDeclaredVars())
+        {
+            String varName = x.getText();
+            if (x.getDeclType() != VarType.FunctionParam
+                    && x.getDeclType() != VarType.FunctionVarDecl)
+            {
+                checked.add(varName);
+                continue;
+            }
+            int foundVarNumber = findVarNumber(varName);
+            if (foundVarNumber != -1
+                    && !checked.contains(varName)) 
+            {
+                table.increaseVarNumber(foundVarNumber + 1,
+                            x.getVarNumber(), varName);
+            }
+            checked.add(varName);
+        }
+
+        this.addAll(table);
     }
 
     /**
-     * Sets the declared vars.
-     * 
-     * @param declaredVars
-     *            the declaredVars to set
+     * Adds declaration to the list without any check.
+     * @param any declaration
      */
-    public void setDeclaredVars(final ArrayList<VarDeclaration> declaredVars) {
-        declaredVars.clear();
-        addAll(declaredVars);
-    }
-
-    public void addToDeclaredVars(final VarDeclaration declaredVar) {
+    public void add(final VarDeclaration declaredVar)
+    {
         declaredVars.add(declaredVar);
     }
 
-    public void addToDeclaredVars(final FunctionDeclaration declaredVar) {
+    /**
+     * Adds declaration to the list without any check.
+     * @param function declaration
+     */
+    public void add(final FunctionDeclaration declaredVar)
+    {
         declaredVars.add(declaredVar);
     }
 
-    public void addToDeclaredVars(final VarUse declaredVar) {
+    /**
+     * Adds declaration to the list without any check.
+     * @param variable usage
+     */
+    public void add(final VarUse declaredVar)
+    {
         declaredVars.add(declaredVar);
     }
 
@@ -169,7 +196,107 @@ public class DeclaredVarsTable {
         else
             return false;
     }
+    
+    private void increaseVarNumber(int onHow, int fromNumber, String varName)
+    {
+        for (VarDeclaration x : getDeclaredVars())
+        {
+            if (x.getText().equals(varName)
+                    && x.getVarNumber() >= fromNumber)
+            {
+                x.setVarNumber(x.getVarNumber() + onHow);
+            }
+            if (!(x instanceof VarUse))
+            {
+                continue;
+            }
+            VarUse varUse = (VarUse) x;
+            if (varUse.getAssignExpr() != null
+                    && varUse.getAssignExpr().getText().equals(varName)
+                    && varUse.getAssignExpr().getVarNumber() >= fromNumber)
+            {
+                varUse.getAssignExpr().setVarNumber(
+                        x.getVarNumber() + onHow);
+            }
+        }
+    }
+    
+    /**
+     * Finds last declaration's var number to determine,
+     * which var actually is in use.
+     * @param Variable name.
+     * @return Largest suitable variable number or 
+     * -1 if there was no declaration at all for this var.
+     */
+    private int findVarNumber(String name)
+    {
+        int maxNum = -1;
+        for (VarDeclaration x : getDeclaredVars())
+        {
+            if (x.getText().equals(name)
+                    && maxNum < x.getVarNumber())
+            {
+                maxNum = x.getVarNumber();
+            }
+        }
 
+        return maxNum;
+    }
+
+
+    public VarDeclaration findDeclaredVar(CommonToken declToken)
+    {
+        for (VarDeclaration x : getDeclaredVars())
+            if (x.getToken().equals(declToken)) 
+                return x;
+        for (VarDeclaration x : getDeclaredVars())
+            if (x instanceof ClassDeclaration) 
+                return x.getDeclaredVars().findDeclaredVar(declToken);
+        return null;
+    }
+    
+    public VarDeclaration findDeclaredVar(String name)
+    {
+        for (VarDeclaration x : getDeclaredVars())
+            if (x.getText().equals(name)
+                    && x.ifVarDeclaration()) 
+                return x;
+        for (VarDeclaration x : getDeclaredVars())
+            if (x instanceof ClassDeclaration) 
+                return x.getDeclaredVars().findDeclaredVar(name);
+        return null;
+    }
+
+    /*
+     * public VarDeclNode findDeclaredVar(String name, CommonToken
+     * blockScopeToken){ for (VarDeclNode x: getDeclaredVars()) if
+     * (x.getScopeToken().equals(blockScopeToken)) return x; for (VarDeclNode x:
+     * getDeclaredVars()) if (x instanceof ClassDeclNode) return
+     * x.getDeclaredVars().findDeclaredVar(token); return null; }
+     */
+    
+    public void setDeclaredVarType(String name, int Num, HaxeType type)
+    {
+        for (VarDeclaration x : getDeclaredVars())
+        {
+            if (Num == x.getVarNumber()
+                    && x.getText().equals(name)
+                    && !x.getDeclType().equals(VarType.ClassVarDecl)) 
+                x.setHaxeType(type);
+        }
+    }
+
+    /**
+     * Sets the declared vars.
+     * 
+     * @param declaredVars
+     *            the declaredVars to set
+     */
+    public void setDeclaredVars(final ArrayList<VarDeclaration> declaredVars)
+    {
+        declaredVars.clear();
+        addAll(declaredVars);
+    }
     public void calculateTypes() {
         boolean ifChanged = false;
         
--- trunk/haXe-IDE-IMP/haXe-IDE/src/haxe/imp/parser/antlr/tree/specific/vartable/VarUse.java
+++ trunk/haXe-IDE-IMP/haXe-IDE/src/haxe/imp/parser/antlr/tree/specific/vartable/VarUse.java
@@ -5,55 +5,83 @@ import org.antlr.runtime.CommonToken;
 import haxe.imp.parser.antlr.tree.HaxeTree;
 import haxe.imp.treeModelBuilder.HaxeTreeModelBuilder.HaxeModelVisitor;
 
-public class VarUse extends VarDeclaration {
-	
-	private HaxeTree fullName;
-	private VarDeclaration assignment = null;
+public class VarUse extends VarDeclaration
+{
 
-	@Override
-	public String getText() {
-		return fullName.getText();
-	}
-	
-	public VarDeclaration getAssignExpr() {
-		return assignment;
-	}
-	
-	public void setAssignExpr(VarDeclaration assignment) {
-		this.assignment = assignment;
-	}
-	
-	public VarUse(HaxeTree name, CommonToken token,int varNumber) {
-		super(token,varNumber);
-		fullName = name;
-	}
-	
-	public void commitIncorrectAssignmentError(){
-		if (assignment.isNil())
-			return;
-		assignment.commitError(assignment.getHaxeType() + " should be " + this.getHaxeType().getTypeName());
-	}
-    
-    public void commitUndeclaredError(){
+    private HaxeTree       fullName;
+    private VarDeclaration assignment = null;
+
+    @Override
+    public String getText()
+    {
+        return fullName.getText();
+    }
+
+    public VarDeclaration getAssignExpr()
+    {
+        return assignment;
+    }
+
+    public void setAssignExpr(VarDeclaration assignment)
+    {
+        this.assignment = assignment;
+    }
+
+    public VarUse(HaxeTree name, CommonToken token, int varNumber) {
+        super(token, varNumber);
+        fullName = name;
+    }
+
+    /**
+     * Creates var use with default variable number = 0.
+     * This means that this variable is the same as in the very
+     * first declaration of variable with such name.
+     * 
+     * @param Name
+     * for variable - it can be complex, so just
+     * copy necessary part of tree.
+     * @param token
+     */
+    public VarUse(HaxeTree name, CommonToken token)
+    {
+        super(token);
+        fullName = name;
+    }
+
+    public void commitIncorrectAssignmentError()
+    {
+        if (assignment.isNil()) return;
+        assignment.commitError(assignment.getHaxeType() 
+                + " should be "
+                + this.getHaxeType().getTypeName());
+    }
+
+    public void commitUndeclaredError()
+    {
         this.commitError(this.getText() + " is not declared.");
     }
-	
-	@Override
-	public void printTree(){
-		if (assignment != null){
-			System.out.println("Assignment");
-			System.out.println("      Name: " + getText() + ", type: "+ getHaxeType().getTypeName()+ 
-	                ", Num: " + getVarNumber());
-			System.out.print("      ");
-			assignment.printTree();
-		}
-		else{
-			System.out.println("UseNode: " + getText() + ", type: "+ getHaxeType().getTypeName()+ 
-	                ", Num: " + getVarNumber());
-		}
-	}
-    
+
     @Override
-    public void accept(final HaxeModelVisitor visitor){
+    public void printTree()
+    {
+        if (assignment != null)
+        {
+            System.out.println("Assignment");
+            System.out.println("      Name: " + getText() + ", type: "
+                    + getHaxeType().getTypeName() 
+                    + ", Num: " + getVarNumber());
+            System.out.print("      ");
+            assignment.printTree();
+        }
+        else
+        {
+            System.out.println("UseNode: " + getText() + ", type: "
+                    + getHaxeType().getTypeName() 
+                    + ", Num: " + getVarNumber());
+        }
     }
+
+    @Override
+    public void accept(final HaxeModelVisitor visitor)
+    {}
 }

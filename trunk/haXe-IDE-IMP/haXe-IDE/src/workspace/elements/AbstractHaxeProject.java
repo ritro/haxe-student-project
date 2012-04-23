d41b9bcfd779558aa86069a3a39792fc8a6705cf
piranuy@gmail.com <piranuy@gmail.com@02e23126-1357-11df-83e8-2f775389245c>
2012-04-23 17:29:33 +0000

    and the rest by hands

---
+++ trunk/haXe-IDE-IMP/haXe-IDE/src/workspace/elements/AbstractHaxeProject.java
@@ -0,0 +1,51 @@
+package workspace.elements;
+
+
+import java.util.HashMap;
+
+import tree.HaxeTree;
+import tree.utils.HaxeTreeLinker;
+
+public abstract class AbstractHaxeProject
+{
+    protected HashMap<String, HaxeFile> fileList = null;
+
+    public AbstractHaxeProject()
+    {
+        fileList = new HashMap<String, HaxeFile>();
+    }
+    
+    public void addFile(final String pack, final HaxeFile file)
+    {
+        fileList.put(pack, file);
+    }
+    
+    public HashMap<String, HaxeFile> getFiles()
+    {
+        return fileList;
+    }
+    
+    /**
+     * Use this notation: a.b.YouFileName
+     */
+    public HaxeFile getFile(final String pack)
+    {
+        return fileList.get(pack);
+    }
+    
+    protected abstract void fillFileList();
+    
+    protected void linkAll()
+    {
+        HaxeTreeLinker linker = new HaxeTreeLinker(this);
+        for (HaxeFile file : fileList.values())
+        {
+            HaxeTree ast = file.getAst();
+            if (ast == null)
+            {
+                continue;
+            }
+            linker.visit(ast);                
+        }
+    }
+}
